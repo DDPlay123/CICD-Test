b4296f1b60f046ed97de510c8fd36ca3b71704e2
DDPlay123 <a70325000@gmail.com>
2024-02-14 03:42:54 +0800

    feat: update appSecret

--- app/src/main/java/mai/project/cicd/MyApp.kt
+++ app/src/main/java/mai/project/cicd/MyApp.kt
@@ -5,13 +5,13 @@ import com.microsoft.appcenter.AppCenter
 import com.microsoft.appcenter.analytics.Analytics
 import com.microsoft.appcenter.crashes.Crashes
 
-class MyApp : Application() {dd
+class MyApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
         AppCenter.start(
             this,
-            "9040ed49-f469-4765-984f-9e2f110eb86e",
+            "6bc21729-ef7c-4f2a-a952-317429183f97",
             // 監測的服務
             Analytics::class.java, Crashes::class.java
         )
